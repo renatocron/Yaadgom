3dd7cc1e7f66284689fd20aeb731458026161f4d
RenatoCRON <renato.cron@gmail.com>
2015-08-31 18:37:47 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Yaadgom - Blah blah blah
+
+# SYNOPSIS
+
+    use Yaadgom;
+
+# DESCRIPTION
+
+Yaadgom is
+
+# AUTHOR
+
+Renato <rentocron@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2015- Renato
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Yaadgom.pm
@@ -0,0 +1,39 @@
+package Yaadgom;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Yaadgom - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Yaadgom;
+
+=head1 DESCRIPTION
+
+Yaadgom is
+
+=head1 AUTHOR
+
+Renato E<lt>rentocron@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Renato
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
